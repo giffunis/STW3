bbca445e5b7a92f3c3dc042c593e8436d2f9e16a
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-10-20 19:12:29 +0100

    añadida el fichero .js de la práctica anterior

---
+++ js/temperatura.js
@@ -0,0 +1,40 @@
+"use stric";
+
+function Medida(){
+  // Declaración de variables
+  this.valor;
+  this.tipo;
+
+  // Funciones de la clase Medida
+  this.getValor = function(){return this.valor;}
+  this.getTipo = function(){return this.tipo;}
+  this.setValor = function(valor){this.valor = valor;}
+  this.setTipo = function(tipo){this.tipo = tipo;}
+  this.set = function(cadena){
+    var regexp = /([+-]?\d+(?:\.\d*)?(?:\s*[e]\d+)?)\s*([fFcC])/;
+    var aux = cadena.match(regexp);
+    this.valor = aux[1];
+    this.tipo = aux[2];
+  }
+}
+
+function Temperatura(){
+  Medida.call(this)
+}
+
+Temperatura.prototype.convert = function(){
+  if(this.tipo === 'C' || this.tipo ==='c'){
+    var aux = this.valor * (9/5) + 32;
+    return (aux.toFixed(1) + 'F');
+  }else{
+    var aux = (this.valor - 32) * (5/9);
+    return (aux.toFixed(1) + 'C');
+  }
+}
+
+
+function convertir(){
+  temp = new Temperatura();
+  temp.set(entrada.value);
+  salida.innerHTML = temp.convert();
+}
