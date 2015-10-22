05260b5edbb97d9143a82de058e725d5ea865ac0
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-10-22 19:54:46 +0100

    Copiado el test.js de la práctica anterior

---
+++ vendor/tests.js
@@ -0,0 +1,23 @@
+var expect = chai.expect;
+
+describe("Pruebas BDD", function() {
+ var aux = document.getElementById("salida");
+
+
+  describe("convertir()", function() {
+    it("convertir(32F)", function() {
+      entrada.value = "32F";
+      convertir();
+      expect(salida.innerHTML).to.equal("0.0C");
+    });
+
+    it("convertir(0C)", function() {
+      entrada.value = "0C";
+      convertir();
+      expect(salida.innerHTML).to.equal("32.0F");
+    });
+
+
+  });
+
+});
