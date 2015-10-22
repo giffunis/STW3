749a64bf7607d39c17b29ae969a18a091d20a1ba
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-10-22 23:32:01 +0100

    añadiendo más test

--- vendor/tests.js
+++ vendor/tests.js
@@ -3,6 +3,19 @@ var expect = chai.expect;
 describe("Pruebas BDD", function() {
  var aux = document.getElementById("salida");
 
+   describe("Temperatura()", function() {
+     it("Temperatura(1 Parametro)", function() {
+         temp = new Temperatura("32F");
+         var out = temp.convert();
+         expect(out).to.equal("0.0C");
+     });
+
+     it("Temperatura(2 Parametros)", function() {
+       temp = new Temperatura("0.0e0","c");
+       var out = temp.convert();
+       expect(out).to.equal("32.0F");
+     });
+   });
 
   describe("convertir()", function() {
     it("convertir(32F)", function() {
@@ -13,7 +26,7 @@ describe("Pruebas BDD", function() {
       };
     });
 
-    it("convertir2(0C)", function() {
+    it("convertir(0C)", function() {
       window.onload = function() {
         entrada.value = "0C";
         convertir();
@@ -21,4 +34,20 @@ describe("Pruebas BDD", function() {
       }
     });
   });
+
+  describe("convert()", function() {
+    it("temp.convert()", function() {
+        temp = new Temperatura("32F");
+        var out = temp.convert();
+        expect(out).to.equal("0.0C");
+    });
+
+    it("convertir(0C)", function() {
+      temp = new Temperatura("0.0e0c");
+      var out = temp.convert();
+      expect(out).to.equal("32.0F");
+    });
+  });
+
+
 });
